776f31f8e0a121f5295bc324727d62de5d85afed
TaxAce-creator <ravena@taxace.com>
2026-09-10 02:50:34 +0000

    Replace @shared path alias with relative imports for Vercel runtime

--- server/routers.ts
+++ server/routers.ts
@@ -1,4 +1,4 @@
-import { COOKIE_NAME } from "@shared/const";
+import { COOKIE_NAME } from "../shared/const.js";
 import { getSessionCookieOptions } from "./_core/cookies.js";
 import { readCookie } from "./auth/security.js";
 import { revokeUserSession } from "./auth/sessionStore.js";
